59f03cce634002a46013bf22eaf947a2e99d5f88
shanaholzmann <shanalucieholzmann@gmail.com>
2024-06-12 19:08:34 +0200

    v1 - character selection within guild

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-// Fetch Shananana
-const fetchShananana = await fetch('https://classic.warcraftlogs.com:443/v1/rankings/character/Shananana/Auberdine/EU?api_key=12d9c0158217c02d36891fced0ddf422');
-const Shananana = await fetchShananana.json();
+async function fetchCharacter(characterSelected) {
+    const response = await fetch('https://classic.warcraftlogs.com:443/v1/rankings/character/' + characterSelected + '/Auberdine/EU?api_key=12d9c0158217c02d36891fced0ddf422');
+    const characterFetched = await response.json();
+    return characterFetched;
+}
 
-function genererPersonnage(character){
-    const infoCharacter = character[0];
+function genererPersonnage(characterFetched) {
+    console.log("function genererpersonnage", characterFetched);
+    const infoCharacter = characterFetched[0];
+    if (!infoCharacter) {
+        console.error("infoCharacter is undefined");
+        return;
+    }
     const charName = infoCharacter.characterName;
     const className = infoCharacter.class;
     const specName = infoCharacter.spec;
@@ -18,42 +25,90 @@ function genererPersonnage(character){
     fichePerso.appendChild(specPerso);
 }
 
-genererPersonnage(Shananana);
-
-function genererParses(character){
-    for (let i = 0; i < character.length; i++){
+function genererParses(character) {
+    for (let i = 0; i < character.length; i++) {
         const parse = character[i];
         const fightName = parse.encounterName;
-        const startFight = parse.startTime;
-        const diffRaid = parse.difficulty;
-        const sizeRaid = parse.size;
-        const ilvlFight = parse.ilvlKeyOrPatch;
+        const encounterID = parse.encounterID;
         const dpsFight = Math.round(parse.total);
         const percentileFight = Math.round(parse.percentile);
 
         const sectionPerso = document.querySelector(".character");
+        const bossName = document.createElement("h3");
+        if (encounterID == 1034 || encounterID == 1029 || encounterID == 1026)
+            bossName.innerHTML = `<i class="fa-solid fa-skull"></i> ${fightName} <i class="fa-solid fa-crown"></i>`;
+        else
+            bossName.innerHTML = `<i class="fa-solid fa-skull"></i> ${fightName}`;
         const inlineResult = document.createElement("p");
-
-        inlineResult.innerText = fightName + " - DPS: " + dpsFight + " - Percentile: " + percentileFight;
-
+        var parseColor = "legendary";
         if (percentileFight < 25)
-            inlineResult.classList.add("poor")
+            parseColor = "poor"
         else if (percentileFight < 50)
-            inlineResult.classList.add("uncommon")
+            parseColor = "uncommon"
         else if (percentileFight < 75)
-            inlineResult.classList.add("rare")
+            parseColor = "rare"
         else if (percentileFight < 95)
-            inlineResult.classList.add("epic")
+            parseColor = "epic"
         else if (percentileFight < 98)
-            inlineResult.classList.add("legendary")
+            parseColor = "legendary"
         else if (percentileFight = 99)
-            inlineResult.classList.add("astounding")
+            parseColor = "astounding"
         else if (percentileFight = 100)
-            inlineResult.classList.add("artifact")
-
+            parseColor = "artifact"
+        inlineResult.innerHTML =`
+        <div class="parse ${parseColor}">
+        <b>DPS</b>: ${dpsFight} - <b>Percentile</b>: ${percentileFight}
+        </div>`;
+        sectionPerso.appendChild(bossName)
         sectionPerso.appendChild(inlineResult)
-        console.log(inlineResult);
     }
 }
 
-genererParses(Shananana);
+const desiredOrder = ["Conclave of Wind", "Al'Akir", "Omnotron Defense System", "Magmaw", "Atramedes", "Chimaeron", "Maloriak", "Nefarian's End", "Halfus Wyrmbreaker", "Theralion and Valiona", "Ascendant Council", "Cho'gall"];
+
+function sortCharacterFetched(characterFetched) {
+    return characterFetched.sort((a, b) => {
+        const indexA = desiredOrder.indexOf(a.encounterName);
+        const indexB = desiredOrder.indexOf(b.encounterName);
+        if (indexA === -1) return 1; // elements not found in the desired order should come last
+        if (indexB === -1) return -1;
+        return indexA - indexB;
+    });
+}
+
+const sectionSelect = document.querySelector(".selection")
+const charSelect = document.createElement("select");
+const characters = ["Keilath", "Shananana", "Adajeed", "Kylania", "Bloodskain", "Møhrzoc", "Ocissa", "Proutzette", "Ravissante", "Ravzed"];
+characters.forEach(character => {
+    const option = document.createElement("option");
+    option.value = character;
+    option.textContent = character;
+    charSelect.appendChild(option);
+});
+
+const textSelect = document.createElement("p");
+textSelect.innerText = "Select a character";
+
+sectionSelect.appendChild(textSelect);
+sectionSelect.appendChild(charSelect);
+
+const boutonSelect = document.createElement("button");
+boutonSelect.innerText = "Load";
+boutonSelect.classList.add("load");
+sectionSelect.appendChild(boutonSelect);
+
+boutonSelect.addEventListener("click", async function () {
+    const sectionPerso = document.querySelector(".character");
+    sectionPerso.innerHTML = '';
+    const characterSelected = charSelect.value;
+    console.log(characterSelected);
+
+    try {
+        const characterFetched = await fetchCharacter(characterSelected);
+        sortCharacterFetched(characterFetched);
+        genererPersonnage(characterFetched);
+        genererParses(characterFetched);
+    } catch (error) {
+        console.error("Failed to fetch character data:", error);
+    }
+});
